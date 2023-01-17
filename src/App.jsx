efbc2dfb6a610dc68fb732ee0dda47cf0f077802
Dayo <michael.abegunde@sendbox.ng>
2023-01-17 22:13:55 +0100

    zircon test functionality implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
+import './styles/App.scss'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import Header from './components/header.jsx'
+import SearchInput from './components/library/search'
+import { TableBodyArray } from './data/tableArray'
+import ComparisonNavBar from './components/ComparisonNavBar';
+import ImageSection from './components/ImageSection';
+import TableSection from './components/TableSection';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hideSame, setHideSame] = useState(false);
+  console.log(Object.keys(TableBodyArray), "yoooo")
 
   return (
     <div className="App">
-      {/* <Header/> */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header/>
+      <div className="body-section">
+        <div>
+          <h2 className='heading'>Compare Schools</h2>
+          <p className='sub-heading'>Compare top four schools you’re interested in and pick the right one for you. </p>
+          <p className='sub-heading'>Use the search field to add schools to compare. You can drag to reorder schools after selection.</p>
+          <SearchInput width={"720px"} className="margin-32"  background={"#ffffff"}/>
+        </div>
+        <ComparisonNavBar/>
+        <ImageSection setHideSame={(e)=> setHideSame(e)} hideSame={hideSame}/>
+        <TableSection hideSame={hideSame}/>
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </div>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as WasabiLogo} from "../assets/wasabi-logo.svg"
+import { WasabiLogo } from '../assets/Wasabilogo';
+import UserLogo from "../assets/user-logo.png";
+import SearchInput from './library/search';
 
 function Header() {
     const navigation = [
@@ -25,12 +27,15 @@ function Header() {
         <div className='header-section'>
             <div className='left-section'>
                 <WasabiLogo/>
-                {navigation.map((item) => (
-                    <p className='nav-text'>{item.title}</p>
+                {navigation.map((item, index) => (
+                    <p className='nav-text'key={index}>{item.title}</p>
                 ))}
             </div>
             <div className='right-section'>
-
+                <SearchInput width={"200px"}/>
+                <div className='profile-image'>
+                    <img src={UserLogo}/>
+                </div>
             </div>
         </div>
 
@@ -46,12 +51,23 @@ const StyledHeader = styled.div`
     align-items: center;
     height: 80px;
     background-color: #ffffff;
+    width: 100%;
     .header-section{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         width: 80%;
     }
     .left-section {
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        min-width: 554px;
+    }
+    .right-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
     .nav-text{
         font-style: normal;
@@ -59,7 +75,39 @@ const StyledHeader = styled.div`
         font-size: 14px;
         line-height: 150%;
         letter-spacing: 0.005em;
+        cursor: pointer;
         color: #062A54;
     }
+    .search-input{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0px 12px;
+        width: 200px;
+        height: 56px;
+        border: 1px solid #CDD4DD;
+        border-radius: 4px;
+        .search{
+            width: 100%;
+            height: 90%;
+            border: none;
+            font-family: Quicksand;
+            ::placeholder{
+                font-style: normal;
+                font-weight: 400;
+                font-size: 14px;
+                letter-spacing: 0.005em;
+                color: #8796A7;
+            }
+            :focus {
+                outline-offset: 0px !important;
+                outline: none !important;
+            }
 
+        }  
+    }
+    .profile-image{
+        margin-left: 20px;
+    }
 `
